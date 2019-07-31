105ba06b11c64f9bb753800de7fd9b76a0827ede
William Skaggs <wskaggs3262@gmail.com>
2019-07-31 16:51:10 -0500

    Remove hello world logging

--- src/main/kotlin/com/skaggsm/treechoppermod/FabricTreeChopper.kt
+++ src/main/kotlin/com/skaggsm/treechoppermod/FabricTreeChopper.kt
@@ -9,10 +9,5 @@ object FabricTreeChopper : ModInitializer {
     const val MODID = "fabric-tree-chopper"
 
     override fun onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-        println("Hello Fabric world!")
     }
 }
