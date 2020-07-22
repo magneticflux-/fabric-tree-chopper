feaf78f3e74278d55042761d718fd448d1604cfd
William Skaggs <wskaggs3262@gmail.com>
2020-07-21 21:04:47 -0500

    Add the automatic conversion of old .toml config file to .json format for fiber

--- src/main/kotlin/com/skaggsm/treechoppermod/FabricTreeChopper.kt
+++ src/main/kotlin/com/skaggsm/treechoppermod/FabricTreeChopper.kt
@@ -6,9 +6,9 @@ import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigBranch
 import io.github.fablabsmc.fablabs.api.fiber.v1.tree.ConfigTree
 import net.fabricmc.api.ModInitializer
 import java.nio.file.Files
-import java.nio.file.OpenOption
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
+import java.util.*
 
 /**
  * Created by Mitchell Skaggs on 7/30/2019.
@@ -17,13 +17,19 @@ object FabricTreeChopper : ModInitializer {
     const val MODID = "fabric-tree-chopper"
 
     val serializer = JanksonValueSerializer(false)
-    val configFile = Path.of("config","testconfig.json")
+    val configFile = Path.of("config","fabric-tree-chopper.json")
+    val oldConfigFile = Path.of("config", "fabric-tree-chopper.toml")
 
     lateinit var config: FabricTreeChopperFiberConfig
     lateinit var configTree: ConfigBranch
 
     override fun onInitialize() {
         config = FabricTreeChopperFiberConfig()
+
+        if (Files.exists(oldConfigFile)) {
+            updateConfigTree()
+        }
+
         configTree = ConfigTree.builder().applyFromPojo(config).build()
 
         if (Files.notExists(configFile)) {
@@ -31,6 +37,25 @@ object FabricTreeChopper : ModInitializer {
         }
 
         deserialize()
+    }
+
+    fun updateConfigTree() {
+        val scan = Scanner(oldConfigFile)
+        val regex = Regex("(.+) = \"?(\\w+)\"?")
+        var line = ""
+        var matchResult: MatchResult
+        while (scan.hasNextLine()) {
+            line = scan.nextLine()
+            matchResult = regex.find(line)!!
+            when (matchResult.groupValues[1]) {
+                "fastLeafDecay" -> {config.fastLeafDecay = matchResult.groupValues[2].toBoolean()}
+                "treeChopMode" -> {config.treeChopMode = ChopMode.valueOf(matchResult.groupValues[2])}
+                "fullChopDurabilityUsage" -> {config.fullChopDurabilityUsage = FullChopDurabilityMode.valueOf(matchResult.groupValues[2])}
+                "sneakToDisable" -> {config.sneakToDisable = matchResult.groupValues[2].toBoolean()}
+                "requireLeavesToChop" -> {config.requireLeavesToChop = matchResult.groupValues[2].toBoolean()}
+            }
+        }
+        Files.delete(oldConfigFile)
     }
 
     fun serialize() {
